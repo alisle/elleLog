da02c5736d0604078f947fb8821074fd320c00af
Alex Lisle <alex.lisle@gmail.com>
2013-01-24 13:40:33 +0000

    Clean up

--- src/ellelog.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"elle"
-)
-
-func main() {
-	ellelog.Run()
-}
--- src/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export GOPATH=$(pwd)/..
-go build ellelog.go
-if [ $? -eq 0 ]; then
-	cp ellelog ../deploy/lib/bin
-fi
